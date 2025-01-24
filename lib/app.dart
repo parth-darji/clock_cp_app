18c9f1e59257cb1e01d537dae7450885f8a2aa40
Parth Darji <parthdarji2961@gmail.com>
2025-01-24 18:49:46 +0530

    removed unnecessary code

--- lib/app.dart
+++ lib/app.dart
@@ -4,7 +4,6 @@ import 'home_page.dart';
 class ClockApp extends StatelessWidget {
   const ClockApp({super.key});
 
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
