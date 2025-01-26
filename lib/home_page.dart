ed2ec8edc6aedce4b0c5754ab9cb5f8db7236eeb
Parth Darji <parthdarji2961@gmail.com>
2025-01-26 13:04:34 +0530

    added some text for showing time

--- lib/home_page.dart
+++ lib/home_page.dart
@@ -1,16 +1,72 @@
 import 'package:clock_cp_app/clock_view.dart';
 import 'package:flutter/material.dart';
+import 'package:intl/intl.dart';
 
 class HomePage extends StatelessWidget {
   const HomePage({super.key});
 
   @override
   Widget build(BuildContext context) {
+    var now = DateTime.now();
+    var formattedTime = DateFormat('HH:mm').format(now);
+    var formattedDate = DateFormat('EEE, d MMM').format(now);
     return Scaffold(
       body: Container(
         alignment: Alignment.center,
+        padding: EdgeInsets.all(32),
         color: Color(0xFF2D2F41),
-        child: ClockView(),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Text(
+              "Clock",
+              style: TextStyle(
+                color: Colors.white,
+                fontSize: 24,
+              ),
+            ),
+            SizedBox(height: 32),
+            Text(
+              formattedTime,
+              style: TextStyle(
+                color: Colors.white,
+                fontSize: 64,
+              ),
+            ),
+            Text(
+              formattedDate,
+              style: TextStyle(
+                color: Colors.white,
+                fontSize: 20,
+              ),
+            ),
+            ClockView(),
+            Text(
+              "Timezone",
+              style: TextStyle(
+                color: Colors.white,
+                fontSize: 24,
+              ),
+            ),
+            SizedBox(height: 16),
+            Row(
+              children: [
+                Icon(
+                  Icons.language,
+                  color: Colors.white,
+                ),
+                SizedBox(width: 16),
+                Text(
+                  "UTC",
+                  style: TextStyle(
+                    color: Colors.white,
+                    fontSize: 24,
+                  ),
+                ),
+              ],
+            ),
+          ],
+        ),
       ),
     );
   }
