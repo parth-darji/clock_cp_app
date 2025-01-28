7f8cae3dc8014f50659127fb39941847ae5e1e33
Parth Darji <parthdarji2961@gmail.com>
2025-01-28 17:28:31 +0530

    if clock is tapped then only the right side screen will visible

--- lib/views/home_page.dart
+++ lib/views/home_page.dart
@@ -41,98 +41,106 @@ class HomePage extends StatelessWidget {
             ),
           ),
           Expanded(
-            child: Container(
-              padding: EdgeInsets.symmetric(
-                horizontal: 32,
-                vertical: 64,
-              ),
-              child: Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: [
-                  SizedBox(height: 20),
-                  Flexible(
-                    flex: 1,
-                    fit: FlexFit.tight,
-                    child: Text(
-                      "Clock",
-                      style: TextStyle(
-                        color: Colors.white,
-                        fontWeight: FontWeight.w700,
-                        fontSize: 24,
-                        fontFamily: "avenir",
-                      ),
-                    ),
+            child: Consumer<MenuInfo>(
+              builder: (context, MenuInfo value, child) {
+                if (value.menuType != MenuType.clock) {
+                  return SizedBox();
+                }
+
+                return Container(
+                  padding: EdgeInsets.symmetric(
+                    horizontal: 32,
+                    vertical: 64,
                   ),
-                  Flexible(
-                    flex: 2,
-                    child: Column(
-                      crossAxisAlignment: CrossAxisAlignment.start,
-                      children: [
-                        Text(
-                          formattedTime,
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      SizedBox(height: 20),
+                      Flexible(
+                        flex: 1,
+                        fit: FlexFit.tight,
+                        child: Text(
+                          "Clock",
                           style: TextStyle(
                             color: Colors.white,
-                            fontSize: 64,
-                            fontFamily: "avenir",
-                          ),
-                        ),
-                        Text(
-                          formattedDate,
-                          style: TextStyle(
-                            color: Colors.white,
-                            fontWeight: FontWeight.w300,
-                            fontSize: 20,
-                            fontFamily: "avenir",
-                          ),
-                        ),
-                      ],
-                    ),
-                  ),
-                  Flexible(
-                    flex: 6,
-                    fit: FlexFit.tight,
-                    child: Align(
-                      alignment: Alignment.center,
-                      child: ClockView(size: 260),
-                    ),
-                  ),
-                  Flexible(
-                    flex: 2,
-                    fit: FlexFit.tight,
-                    child: Column(
-                      crossAxisAlignment: CrossAxisAlignment.start,
-                      children: [
-                        Text(
-                          "Timezone",
-                          style: TextStyle(
-                            color: Colors.white,
-                            fontWeight: FontWeight.w500,
+                            fontWeight: FontWeight.w700,
                             fontSize: 24,
                             fontFamily: "avenir",
                           ),
                         ),
-                        SizedBox(height: 16),
-                        Row(
+                      ),
+                      Flexible(
+                        flex: 2,
+                        child: Column(
+                          crossAxisAlignment: CrossAxisAlignment.start,
                           children: [
-                            Icon(
-                              Icons.language,
-                              color: Colors.white,
-                            ),
-                            SizedBox(width: 16),
                             Text(
-                              "UTC$offsetSign$timezoneString",
+                              formattedTime,
                               style: TextStyle(
                                 color: Colors.white,
-                                fontSize: 14,
+                                fontSize: 64,
+                                fontFamily: "avenir",
+                              ),
+                            ),
+                            Text(
+                              formattedDate,
+                              style: TextStyle(
+                                color: Colors.white,
+                                fontWeight: FontWeight.w300,
+                                fontSize: 20,
+                                fontFamily: "avenir",
                               ),
                             ),
                           ],
                         ),
-                      ],
-                    ),
+                      ),
+                      Flexible(
+                        flex: 6,
+                        fit: FlexFit.tight,
+                        child: Align(
+                          alignment: Alignment.center,
+                          child: ClockView(size: 260),
+                        ),
+                      ),
+                      Flexible(
+                        flex: 2,
+                        fit: FlexFit.tight,
+                        child: Column(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: [
+                            Text(
+                              "Timezone",
+                              style: TextStyle(
+                                color: Colors.white,
+                                fontWeight: FontWeight.w500,
+                                fontSize: 24,
+                                fontFamily: "avenir",
+                              ),
+                            ),
+                            SizedBox(height: 16),
+                            Row(
+                              children: [
+                                Icon(
+                                  Icons.language,
+                                  color: Colors.white,
+                                ),
+                                SizedBox(width: 16),
+                                Text(
+                                  "UTC$offsetSign$timezoneString",
+                                  style: TextStyle(
+                                    color: Colors.white,
+                                    fontSize: 14,
+                                  ),
+                                ),
+                              ],
+                            ),
+                          ],
+                        ),
+                      ),
+                    ],
                   ),
-                ],
-              ),
+                );
+              },
             ),
           ),
         ],
