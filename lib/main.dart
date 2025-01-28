0b6b17e980f6c58c9ba6a1b9dba9b0ec91a3108b
Parth Darji <parthdarji2961@gmail.com>
2025-01-28 21:58:40 +0530

    setup the flutter local notification (ios tested)

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,33 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_local_notifications/flutter_local_notifications.dart';
 
 import 'app.dart';
 
-void main() {
+final FlutterLocalNotificationsPlugin flutterLocalNotificationsPlugin =
+    FlutterLocalNotificationsPlugin();
+
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+
+  var initializationSettingsAndroid =
+      AndroidInitializationSettings('codex_logo');
+  var initializationSettingsIOS = DarwinInitializationSettings(
+    requestAlertPermission: true,
+    requestBadgePermission: true,
+    requestSoundPermission: true,
+  );
+  var initializationSettings = InitializationSettings(
+    android: initializationSettingsAndroid,
+    iOS: initializationSettingsIOS,
+  );
+  await flutterLocalNotificationsPlugin.initialize(
+    initializationSettings,
+    onDidReceiveNotificationResponse: (details) {
+      if (details.payload != null) {
+        debugPrint('notification payload: ${details.payload!}');
+      }
+    },
+  );
+
   runApp(const ClockApp());
 }
